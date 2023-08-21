963e6e7c2a59f08cd709c12955f64b77ff4f25e1
NicolauSouza21 <algumacoisan@gmail.com>
2023-08-21 05:00:40 -0300

    21/08

---
+++ js/Carrinho.js
@@ -0,0 +1,33 @@
+const items = [{
+
+    id:0 ,
+    nome: 'tenis',
+    img: 'image.jpeg',
+    quantidade: 0
+
+
+},
+{
+
+    id:1 ,
+    nome: 'tenis',
+    img: 'image.jpeg',
+    quantidade: 0
+
+
+},
+ {
+
+    id:2 ,
+    nome: 'tenis',
+    img: 'image.jpeg',
+    quantidade: 0
+
+
+}
+]
+
+lojainit = () => {
+var containerProdutos = document.getElementById('produtos');
+
+}
